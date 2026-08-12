0d425aef3b59f236d0998cd436723c09e4a0ccbd
Ashish <ashish@lifesciencetrust.com>
2026-08-12 11:41:17 +0530

    Remove top announcement bar

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -5,7 +5,6 @@ import WhatsAppFloat from "@/components/WhatsAppFloat";
 export default function SiteLayout({ children }) {
   return (
     <>
-      <div className="announce">FREE PAN INDIA DELIVERY &nbsp;|&nbsp; CASH ON DELIVERY AVAILABLE &nbsp;|&nbsp; EASY 7-DAY RETURNS</div>
       <Header />
       {children}
       <Footer />
